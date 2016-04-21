127f66792432e436e714f6a7006dcfafbce9f91b
wang-q <wang-q@outlook.com>
2016-04-21 22:37:00 +0800

    Huge file tests. 3x faster than xs version and 5x memory usages.
    $ time java -jar target/jintspan-0.1.1-SNAPSHOT.jar file
    step 1 create
    duration 0.389613
    step 2 intersect
    duration 3.489330
    step 3 intersect runlist
    duration 3.511667
    
    real	0m7.493s
    user	0m7.773s
    sys	0m0.132s
    
    $ time perl test_ai.pl file xs
    ==> test against large sets
    step 1 create
    duration 0.097701
    step 2 intersect
    duration 9.063975
    step 3 intersect runlist
    duration 9.227608
    
    real	0m18.483s
    user	0m18.414s
    sys	0m0.050s
    
    $ time perl test_ai.pl file
    ==> test against large sets
    step 1 create
    duration 3.920457
    step 2 intersect
    duration 52.694376
    step 3 intersect runlist
    duration 52.964269
    
    real	1m49.674s
    user	1m49.519s
    sys	0m0.101s

--- src/main/java/com/github/egateam/IntSpanBenchmark.java
+++ src/main/java/com/github/egateam/IntSpanBenchmark.java
@@ -6,23 +6,100 @@
 
 package com.github.egateam;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Objects;
 
 @SuppressWarnings("WeakerAccess")
 public class IntSpanBenchmark {
 
+    // don't use Jackson to avoid adding dependence
     private static class RunFile {
+        void run() {
+            String r1 = readFile("/r1.yml");
+            String r2 = readFile("/r2.yml");
 
+            r1 = r1.replaceFirst("---\\s+1: ", "").replaceAll("\\n", "");
+            r2 = r2.replaceFirst("---\\s+1: ", "").replaceAll("\\n", "");
+
+            IntSpan set1;
+            IntSpan set2;
+
+            int step = 1;
+            {
+                System.out.printf("step %d create\n", step);
+                step++;
+
+                long start = System.nanoTime();
+                for ( int i = 1; i <= 100; i++ ) {
+                    set1 = new IntSpan(r1);
+                    set2 = new IntSpan(r2);
+                }
+                long end = System.nanoTime();
+
+                System.out.printf("duration %f\n", (end - start) / 1000.0 / 1000.0 / 1000.0);
+            }
+
+            {
+                System.out.printf("step %d intersect\n", step);
+                step++;
+
+                long start = System.nanoTime();
+                for ( int i = 1; i <= 1000; i++ ) {
+                    set1 = new IntSpan(r1);
+                    set2 = new IntSpan(r2);
+                    set1.intersect(set2);
+                }
+                long end = System.nanoTime();
+
+                System.out.printf("duration %f\n", (end - start) / 1000.0 / 1000.0 / 1000.0);
+            }
+
+            {
+                System.out.printf("step %d intersect runlist\n", step);
+
+                long start = System.nanoTime();
+                for ( int i = 1; i <= 1000; i++ ) {
+                    set1 = new IntSpan(r1);
+                    set2 = new IntSpan(r2);
+                    set1.intersect(set2).runlist();
+                }
+                long end = System.nanoTime();
+
+                System.out.printf("duration %f\n", (end - start) / 1000.0 / 1000.0 / 1000.0);
+            }
+
+        }
+
+        String readFile(String filename) {
+            String content = "";
+            InputStream in = getClass().getResourceAsStream(filename);
+
+            try ( BufferedReader reader = new BufferedReader(new InputStreamReader(in)) ) {
+                String line;
+                while ( (line = reader.readLine()) != null ) {
+                    content += line;
+                }
+
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+            return content;
+        }
     }
+
     private static class RunBenchmark {
         void run() {
             for ( int i : new int[]{2, 3, 4, 5, 6} ) {
-                System.out.print(String.format("step %d\n", i));
+                System.out.printf("step %d\n", i);
                 long start = System.nanoTime();
                 testAddRange(i);
                 long end = System.nanoTime();
-                System.out.print(String.format("duration %f\n", (end - start) / 1000.0 / 1000.0 / 1000.0));
+                System.out.printf("duration %f\n", (end - start) / 1000.0 / 1000.0 / 1000.0);
 
             }
         }
@@ -96,6 +173,8 @@ public class IntSpanBenchmark {
             System.err.print(usage);
         } else if ( Objects.equals(args[0], "benchmark") ) {
             new RunBenchmark().run();
+        } else if ( Objects.equals(args[0], "file") ) {
+            new RunFile().run();
         } else {
             System.err.printf("Unrecognized command %s", args[0]);
         }
